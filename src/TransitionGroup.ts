785e55fa1cfd01bd227d8d6d6165d4b11a76426c
Xavier Loh <xavier.loh.yz@gmail.com>
2021-10-01 15:17:47 +0800

    Don't call initial enter if there are no elements.

--- src/TransitionGroup.ts
+++ src/TransitionGroup.ts
@@ -48,49 +48,50 @@ export const TransitionGroup: Component<TransitionGroupProps> = (props) => {
     schedule(requestAnimationFrame, () => {
       if (isInitial) {
         isInitial = false;
+        if (!els.length) return;
         if (typeof enterInitial === "function") enterInitial(els);
         else if (enterInitial === true && enter) enter(els);
         else if (enterInitial !== false && enter?.initial) enter.initial(els);
         setEls(els);
-      } else {
-        const prevEls = untrack(getEls);
-
-        if (enter) {
-          const enteringEls = els.filter((el) => !prevElSet.has(el));
-          enteringEls.length && enter(enteringEls);
-        }
-
-        if (exit) {
-          // Modify prevElSet in place since we have no more use for it
-          const exitingElSet = prevElSet;
-          for (const el of exitingElSet)
-            elSet.has(el) && exitingElSet.delete(el);
-          // Persist previous els; they will be removed by removeEls
-          for (let i = 0; i < prevEls.length; i++)
-            !elSet.has(prevEls[i]) && els.splice(i, 0, prevEls[i]);
-          if (exitingElSet.size) {
-            // We have els exiting
-            const exitingEls = [...exitingElSet];
-            const removeEls = (removedEl?: StylableElement) => {
-              setEls((prevEls) => {
-                const els = prevEls.filter((el) =>
-                  removedEl ? el !== removedEl : !exitingElSet.has(el)
-                );
-                move && els.length && move(els);
-                return els;
-              });
-            };
-            exit(exitingEls, removeEls);
-          }
-        }
-
-        if (move) {
-          const movingEls = prevEls;
-          movingEls.length && move(movingEls);
-        }
-
-        setEls(els);
+        return;
       }
+
+      const prevEls = untrack(getEls);
+
+      if (enter) {
+        const enteringEls = els.filter((el) => !prevElSet.has(el));
+        enteringEls.length && enter(enteringEls);
+      }
+
+      if (exit) {
+        // Modify prevElSet in place since we have no more use for it
+        const exitingElSet = prevElSet;
+        for (const el of exitingElSet) elSet.has(el) && exitingElSet.delete(el);
+        // Persist previous els; they will be removed by removeEls
+        for (let i = 0; i < prevEls.length; i++)
+          !elSet.has(prevEls[i]) && els.splice(i, 0, prevEls[i]);
+        if (exitingElSet.size) {
+          // We have els exiting
+          const exitingEls = [...exitingElSet];
+          const removeEls = (removedEl?: StylableElement) => {
+            setEls((prevEls) => {
+              const els = prevEls.filter((el) =>
+                removedEl ? el !== removedEl : !exitingElSet.has(el)
+              );
+              move && els.length && move(els);
+              return els;
+            });
+          };
+          exit(exitingEls, removeEls);
+        }
+      }
+
+      if (move) {
+        const movingEls = prevEls;
+        movingEls.length && move(movingEls);
+      }
+
+      setEls(els);
     });
 
     return elSet;
